fe2168ea342dc0ec622674c869306b64e84a3a6c
Akhil Puri <ap4723@srmist.edu.in>
2026-01-16 19:19:24 +0530

    Updated: package name

---
+++ src/coreprogramming/programmingelements/level1/DividePen.java
@@ -0,0 +1,14 @@
+package coreprogramming.programmingelements.level1;
+
+public class DividePen {
+    public static void main(String[] args){
+        int pens = 14;
+        int students = 3;
+
+        int quantity = pens / students;
+        int remaining = pens % students;
+
+        System.out.println("The Pen Per Student is " + quantity + " " +
+                "and the remaining pen not distributed is " + remaining);
+    }
+}
---
+++ src/coreprogramming/programmingelements/practice_problem/Welcome.java
@@ -0,0 +1,7 @@
+package coreprogramming.programmingelements.practice_problem;
+
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("Welcome to Bridgelabz!");
+    }
+}
