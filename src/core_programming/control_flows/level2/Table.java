8167485d5192c5956d9987ada3a4333a4d0d1d5c
Akhil Puri <ap4723@srmist.edu.in>
2026-01-16 13:24:41 +0530

    completed core-programming

---
+++ src/core_programming/control_flows/level2/Table.java
@@ -0,0 +1,16 @@
+package core_programming.control_flows.level2;
+
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int number = sc.nextInt();
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
